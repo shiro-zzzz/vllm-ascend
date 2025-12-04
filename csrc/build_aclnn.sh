51aee08eaefc76f16442044fa63d14a9f5fe0981
shiro-zzzz <zhangdianhao@huawei.com>
2025-12-04 14:36:42 +0800

    [Kernel] Add moe normal ops 1.Add the implementation of normal Aclnn operators: CamMoeCombineNormal, CamMoeDispatchNormal, NotifyDispatch，and DispatchLayout. 2.Provide PyTorch interfaces for the normal operators: get_dispatch_layout, dispatch_prefill, and combine_prefill.

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -15,7 +15,16 @@ elif [[ "$SOC_VERSION" =~ ^ascend910b ]]; then
     SOC_ARG="ascend910b"
 elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
     # ASCEND910C (A3) series
-    CUSTOM_OPS="grouped_matmul_swiglu_quant_weight_nz_tensor_list;lightning_indexer;sparse_flash_attention"
+    CUSTOM_OPS_ARRAY=(
+        "grouped_matmul_swiglu_quant_weight_nz_tensor_list"
+        "lightning_indexer"
+        "sparse_flash_attention"
+        "cam_moe_combine_normal"
+        "cam_moe_dispatch_normal"
+        "dispatch_layout"
+        "notify_dispatch"
+    )
+    CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
     SOC_ARG="ascend910_93"
 else
     # others
@@ -27,7 +36,7 @@ fi
 cd csrc
 rm -rf build output
 echo "building custom ops $CUSTOM_OPS for $SOC_VERSION"
-bash build.sh -n $CUSTOM_OPS -c $SOC_ARG
+bash build.sh -n "$CUSTOM_OPS" -c "$SOC_ARG"
 
 # install custom ops to vllm_ascend/_cann_ops_custom
 ./output/CANN-custom_ops*.run --install-path=$ROOT_DIR/vllm_ascend/_cann_ops_custom
